b6579c05f18b1f4e3eda2c688298249373eba653
chensui <simple1115@gmail.com>
2019-12-22 18:48:18 +0800

    get localip

--- src/frostyraft.c
+++ src/frostyraft.c
@@ -8,6 +8,6 @@ static char myipv4[16];
 int main(int argc, char** argv) 
 {
 	sk_get_local_ipv4("eth0", myipv4);
-	SCREEN(SCREEN_GREEN, stdout, "%s start frostyraft nows", myipv4);
+	SCREEN(SCREEN_GREEN, stdout, "%s start frostyraft now", myipv4);
 	return 0;
 }
