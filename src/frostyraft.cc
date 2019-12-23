d2fdad846e310fc3b8c7cd1d253e1baaecbc40f4
frost <simple1115@gmail.com>
2019-12-23 17:32:13 +0800

    init failure

--- src/frostyraft.cc
+++ src/frostyraft.cc
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include "logger.h"
 #include "raft.h"
@@ -7,7 +8,7 @@
 int main(int argc, char** argv) 
 {
 	raft_t r;
-	init_raft(&r);
+	if (init_raft(&r) != 0) exit(EXIT_FAILURE);
 	
 	info("%s %s start frostyraft now", r.ipv4, r.uuid);
 	return 0;
